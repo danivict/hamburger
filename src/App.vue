4939fd5f31f3347f41606e66ba30d6c729177c92
Daniel Lemos <danivictor45i@gmail.com>
2022-07-08 15:45:47 -0300

    aula 5 - criando primeiro componente

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <first-component/>
+</template>
+
+<script>
+    import FirstComponent from './components/FirstComponent.vue'
+
+    export default {
+        name: 'App',
+        components: {
+            FirstComponent,
+        }
+    }
+</script>
